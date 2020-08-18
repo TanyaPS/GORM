13a44457bb6cb52c10925c6cb2ea6cd7ea3fc2d7
Tanya Sunding <tpsunding@yahoo.dk>
2020-08-18 10:22:29 +0200

    review hours2days

--- bin/hours2days.py
+++ bin/hours2days.py
@@ -27,6 +27,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 from pathlib import Path
+import socket
 
 import configparser
 
@@ -48,6 +49,9 @@ path_to_files = path + foldername
 logfilename = parser.get("setup", "logfilename")
 logfile_path = path + logfilename
 
+local_name = socket.gethostname()
+local_host = parser.get("Local", "host")
+local_user = parser.get("Local", "user")
 
 last_filename = ""
 os.system("mkdir -m777 -p %s" % (path_to_files))
@@ -83,7 +87,7 @@ ftp.cwd(current_year)  # current year directory
 filelist = ftp.nlst()
 
 
-def GetMove(filename, path_to_files):
+def get_move(filename, path_to_files):
     # gets file from ftp server and place in folder
     fhandle = open(filename, "wb")
     ftp.retrbinary("RETR " + filename, fhandle.write)
@@ -98,7 +102,7 @@ def GetMove(filename, path_to_files):
     os.system("gunzip %s/%s" % (path_to_files, filename))
 
 
-def MergeUpload(filename, path_to_files):
+def merge_upload(filename, path_to_files):
     station_doy = filename[0:-8]
     yr = filename[-6:-4]
     file_type = filename[11]
@@ -139,7 +143,7 @@ def MergeUpload(filename, path_to_files):
     f.close
 
 
-def UnfinishedDays(last_filename, path_to_files):
+def unfinished_days(last_filename, path_to_files):
     # moves and zips unfinished days
     last_file_type = last_filename[11]
     last_yr = last_filename[-6:-4]
@@ -167,13 +171,13 @@ def UnfinishedDays(last_filename, path_to_files):
     logger.info(last_station_doy + " is unfinished")
 
 
-def ListMerge(list_of_files, path_to_files, doy):
-    list_of_files.append("testtest.000.gz")
+def list_merge(list_of_files, path_to_files, doy):
+    list_of_files.append("listender.000.gz") #add extra name to list, so last station/doy is included
     last_filename = ""
     doy_list = []
     for filename in list_of_files:
         if filename != list_of_files[-1]:
-            GetMove(filename, path_to_files)
+            get_move(filename, path_to_files)
         locationName = filename[0:-8]
         if (
             last_filename and last_filename[0:-8] == locationName
@@ -184,7 +188,7 @@ def ListMerge(list_of_files, path_to_files, doy):
                 and len(doy_list) == 24
             ):  # last file of finished doy
                 # merge files
-                MergeUpload(filename, path_to_files)
+                merge_upload(filename, path_to_files)
                 # delete files locally
                 os.system(
                     r"find  %s/%s[a-z]%s -maxdepth 1 -type f -delete"
@@ -197,7 +201,7 @@ def ListMerge(list_of_files, path_to_files, doy):
                 and len(doy_list) < 24
                 and last_filename[4:7] != str(doy)
             ):
-                UnfinishedDays(last_filename, path_to_files)
+                unfinished_days(last_filename, path_to_files)
                 os.system(
                     r"find  %s/%s[a-z]%s -maxdepth 1 -type f -delete"
                     % (path_to_files, last_filename[0:7], last_filename[8:12])
@@ -212,74 +216,80 @@ def ListMerge(list_of_files, path_to_files, doy):
 
         last_filename = filename
 
+def main():
 
-obs_list = []
-GPS_list = []
-GLO_list = []
-for filename in filelist:
-    if re.match(r".*[a-xA-X]{1}\.[0-9]{2}[gno]\.gz$", filename):
-        file_type = filename[11]
-        # get file
-        # GetMove(filename, path_to_files)
-        if file_type == "o":
-            obs_list.append(filename)
+    obs_list = []
+    GPS_list = []
+    GLO_list = []
+    for filename in filelist:
+        if re.match(r".*[a-xA-X]{1}\.[0-9]{2}[gno]\.gz$", filename):
+            file_type = filename[11]
+            # get file
+            # GetMove(filename, path_to_files)
+            if file_type == "o":
+                obs_list.append(filename)
 
-        elif file_type == "n":
-            GPS_list.append(filename)
+            elif file_type == "n":
+                GPS_list.append(filename)
 
-        elif file_type == "g":
-            GLO_list.append(filename)
+            elif file_type == "g":
+                GLO_list.append(filename)
 
-# merge obs hour files to days
-ListMerge(obs_list, path_to_files, doy)
+    # merge obs hour files to days
+    list_merge(obs_list, path_to_files, doy)
 
-# merge GPS nav hour files to days
-ListMerge(GPS_list, path_to_files, doy)
+    # merge GPS nav hour files to days
+    list_merge(GPS_list, path_to_files, doy)
 
-# merge GLONASS nav hour files to days
-ListMerge(GLO_list, path_to_files, doy)
+    # merge GLONASS nav hour files to days
+    list_merge(GLO_list, path_to_files, doy)
 
-# Delete files locally (possibly large number of files)
-# if still too many files, divide into further groups
-os.system(r"find  %s/[a-l]*[0-9][o] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[m-z]*[0-9][o] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[a-l]*[0-9][d] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[m-z]*[0-9][d] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[a-l]*[0-9][n] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[m-z]*[0-9][n] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[a-l]*[0-9][g] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(r"find  %s/[m-z]*[0-9][g] -maxdepth 1 -type f -delete" % (path_to_files))
-os.system(
-    r"find  %s/[a-f]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
-)
-os.system(
-    r"find  %s/[g-m]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
-)
-os.system(
-    r"find  %s/[n-q]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
-)
-os.system(
-    r"find  %s/[r-z]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
-)
-
-ftp.quit()
-
-# monitor disk usage
-batcmd = "df --total"
-dfresult = subprocess.check_output(batcmd, shell=True)
-disk_usage = re.findall(r"total\s.*$", dfresult)[0]
-disk_usage = disk_usage.split(" ")[-2]
-used_float = float(disk_usage.strip("%"))
-if used_float >= 80:
-    logger.warning("gpsftp6 disk usage " + disk_usage)
+    # Delete files locally (possibly large number of files)
+    # if still too many files, divide into further groups
+    
+    os.system(r"find  %s/[a-l]*[0-9][o] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[m-z]*[0-9][o] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[a-l]*[0-9][d] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[m-z]*[0-9][d] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[a-l]*[0-9][n] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[m-z]*[0-9][n] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[a-l]*[0-9][g] -maxdepth 1 -type f -delete" % (path_to_files))
+    os.system(r"find  %s/[m-z]*[0-9][g] -maxdepth 1 -type f -delete" % (path_to_files))
     os.system(
-        r"echo \"sdfe1h2dayfiles_EXT.py: disk usage on gpsftp6 is %s\" | mail -s \"gpsftp6_disk_usage_warning\" -r gpsuser@gpsftp6.prod.sitad.dk taphs@sdfe.dk"
-        % (disk_usage)
+        r"find  %s/[a-f]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
+    )
+    os.system(
+        r"find  %s/[g-m]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
+    )
+    os.system(
+        r"find  %s/[n-q]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
+    )
+    os.system(
+        r"find  %s/[r-z]*[0-9][odgnP]\.gz -maxdepth 1 -type f -delete" % (path_to_files)
     )
 
+    ftp.quit()
 
-if newlog:
-    os.system(
-        r"mail  -s \"sdfe1h2dayfiles_EXT_warning\" -r gpsuser@gpsftp6.prod.sitad.dk %s < %s"
-        % (email_to, logfile_path)
-    )
+    # monitor disk usage
+    batcmd = "df --total"
+    dfresult = subprocess.check_output(batcmd, shell=True)
+    disk_usage = re.findall(r"total\s.*$", dfresult)[0]
+    disk_usage = disk_usage.split(" ")[-2]
+    used_float = float(disk_usage.strip("%"))
+    if used_float >= 80:
+        logger.warning(local_name + " disk usage " + disk_usage)
+        os.system(
+            r"echo \"%s: disk usage on %s is %s\" | mail -s \"%s_disk_usage_warning\" -r %s@%s %s"
+            % (__file__, local_name, local_name, local_user, local_host, disk_usage, email_to)
+        )
+
+
+    if newlog:
+        os.system(
+            r"mail  -s \"%s_warning\" -r %s@%s %s < %s"
+            % (__file__, local_user, local_host, email_to, logfile_path)
+        )
+
+if __name__ == "__main__":
+    # køres kun når scriptet kaldes fra kommandolinjen
+    main()
